ce295bd8c23895c951d588dadf16aa52a4198d80
bt597809 <divakarababu.gaddam@gmail.com>
2021-11-28 03:13:40 +0800

    Add files via upload

---
+++ BinaryTreeProblem.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelepathyLabsTest
+{
+    class MathExpression : ICommand
+    {
+        public static MathExpression Instance { get; } = new MathExpression();
+
+        private MathExpression()
+        {
+        }
+        static nptr newNode(string c)
+        {
+            nptr n = new nptr();
+            n.data = c;
+            n.left = n.right = null;
+            return n;
+        }
+
+        static nptr build(String s)
+        {
+            try
+            {
+                Stack<nptr> stN = new Stack<nptr>();
+
+                Stack<char> stC = new Stack<char>();
+                nptr t, t1, t2 = null;
+
+                int[] p = new int[123];
+                p['+'] = p['-'] = 1;
+                p['/'] = p['*'] = 2;
+                p[')'] = 0;
+
+                string num = string.Empty;
+                for (int i = 0; i < s.Length; i++)
+                {
+                    //if(char.IsDigit(s[i]))
+                    //{
+                    //    num += s[i].ToString();
+                    //    if(i + 1 < s.Length && char.IsDigit(s[i + 1]))
+                    //    {
+                    //        continue;
+                    //    }
+                    //}
+
+
+                    if (s[i] == '(')
+                    {
+                        stC.Push(s[i]);
+                    }
+
+                    else if (char.IsDigit(s[i]))
+                    {
+                        t = newNode(s[i].ToString());
+                        stN.Push(t);
+                        num = string.Empty;
+                    }
+                    else if (p[s[i]] > 0)
+                    {
+
+                        while (stC.Count != 0 && stC.Peek() != '('
+                            && ((p[stC.Peek()] >= p[s[i]])
+                                || (p[stC.Peek()] > p[s[i]])))
+                        {
+
+                            t = newNode(stC.Peek().ToString());
+                            stC.Pop();
+
+                            t1 = stN.Peek();
+                            stN.Pop();
+
+                            if(stN.Count > 0)
+                            {
+                                t2 = stN.Peek();
+                                stN.Pop();
+                            }
+                            
+                            t.left = t2;
+                            t.right = t1;
+
+                            stN.Push(t);
+                        }
+
+                        stC.Push(s[i]);
+                    }
+                    else if (s[i] == ')')
+                    {
+                        while (stC.Count != 0 && stC.Peek() != '(')
+                        {
+                            t = newNode(stC.Peek().ToString());
+                            stC.Pop();
+                            t1 = stN.Peek();
+                            stN.Pop();
+                            t2 = stN.Peek();
+                            stN.Pop();
+                            t.left = t2;
+                            t.right = t1;
+                            stN.Push(t);
+                        }
+                        stC.Pop();
+                    }
+                }
+                t = stN.Peek();
+                return t;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        static void postorder(nptr root)
+        {
+            if (root != null)
+            {
+                postorder(root.left);
+                postorder(root.right);
+                Console.Write(root.data);
+            }
+        }
+
+        public void ProcessCommand(string command)
+        {
+            if (command == GlobalConstants.EXIT)
+            {
+                Environment.Exit(0);
+            }
+            else
+            {
+                var math = MathExpression.Instance;
+                var inputs = command.Split(' ');
+                var cmd = inputs[0];
+                var expression = inputs.Length > 1 ? inputs[1] : "((5/(7-(1+1)))*3)-(2+(1+1))";
+                switch (cmd)
+                {
+                    case "run":
+                        math.run(expression);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid command");
+                        break;
+                }
+            }
+        }
+
+        private void run(string expression)
+        {
+            expression = "(" + expression + ")";
+            nptr root = build(expression);
+
+            // Function call
+            postorder(root);
+            BTreePrinter.Print(root);
+            Console.WriteLine(evalTree(root));
+        }
+
+        public int evalTree(nptr root)
+        {
+
+            // Empty tree
+            if (root == null)
+                return 0;
+
+            // Leaf node i.e, an integer
+            if (root.left == null && root.right == null)
+                return Convert.ToInt32(root.data);
+
+            // Evaluate left subtree
+            int leftEval = evalTree(root.left);
+
+            // Evaluate right subtree
+            int rightEval = evalTree(root.right);
+
+            // Check which operator to apply
+            if (root.data == "+")
+                return leftEval + rightEval;
+
+            if (root.data == "-")
+                return leftEval - rightEval;
+
+            if (root.data == "*")
+                return leftEval * rightEval;
+
+            return leftEval / rightEval;
+        }
+    }
+
+    public class nptr
+    {
+        public string data;
+        public nptr left, right;
+    };
+}
---
+++ Program.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelepathyLabsTest
+{
+    class Program
+    {
+        const string EXIT = "exit";
+        static ICommand module;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter input: 1 or 2");
+                Console.WriteLine("1 -> Boutique Hotel");
+                Console.WriteLine("2 -> Math operations");
+                Console.WriteLine("Input exit to close the application");
+
+                var command = Console.ReadLine().ToLower();
+                if (!string.IsNullOrWhiteSpace(command))
+                {
+                    if (command == EXIT)
+                    {
+                        Environment.Exit(0);
+                    }
+                    else
+                    {
+                        if(command == "1")
+                        {
+                            Console.WriteLine("Commands list:");
+                            Console.WriteLine(" list (lists all available rooms)");
+                            Console.WriteLine(" book (books the nearest available room from the entrance)");
+                            Console.WriteLine(" checkout 1A (checks out the room)");
+                            Console.WriteLine(" clean 1A (cleans the room)");
+                            Console.WriteLine(" repair 1A (sets the room to repair mode)");
+                            Console.WriteLine(" fix 1A (repairs and sets the room to vacant)");
+                            Console.WriteLine(" exit (quits the application)");
+                            module = Hotel.Instance;
+                        }
+                        else if(command == "2")
+                        {
+                            Console.WriteLine("Commands list");
+                            Console.WriteLine(" run (1*2)+3");
+                            module = MathExpression.Instance;
+                        }
+
+                        if (module != null)
+                        {
+                            Prompt();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid Command");
+                            Prompt();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Unknown error, please try again");
+                Console.ReadLine();
+            }
+        }
+
+        static void Prompt()
+        {
+            Console.WriteLine("Enter input:");
+            var command = Console.ReadLine().ToLower();
+            module.ProcessCommand(command);
+            Prompt();
+        }
+    }
+
+    //class MathExpression : ICommand
+    //{
+    //    public static MathExpression Instance { get; } = new MathExpression();
+
+    //    private MathExpression()
+    //    {
+    //    }
+    //    public void ProcessCommand(string command)
+    //    {
+    //        if (command == GlobalConstants.EXIT)
+    //        {
+    //            Environment.Exit(0);
+    //        }
+    //        else
+    //        {
+                
+    //        }
+    //    }
+    //}
+
+    class Hotel: ICommand
+    {
+        private string _roomsSample = "1a,1b,1c,1d,1e,2e,2d,2c,2b,2a,3a,3b,3c,3d,3e,4e,4d,4c,4b,4a";
+        public Dictionary<string, Status> Rooms { get; set; }
+        public static Hotel Instance { get; } = new Hotel();
+
+        private Hotel()
+        {
+            Rooms = new Dictionary<string, Status>();
+            foreach (var room in _roomsSample.Split(','))
+            {
+                Rooms.Add(room, Status.Available);
+            }
+        }
+
+        void Display()
+        {
+            var availableRooms = Rooms.Where(p => p.Value == Status.Available).Select(p => p.Key);
+            Console.WriteLine(string.Join(", ", availableRooms));
+        }
+
+        void Book()
+        {
+            KeyValuePair<string, Status> room;
+            string bookedRoom = null;
+            for (int i = 0; i < Rooms.Count; i++)
+            {
+                room = Rooms.ElementAt(i);
+                if (room.Value == Status.Available)
+                {
+                    Rooms[room.Key] = Status.Occupied;
+                    bookedRoom = room.Key;
+                    break;
+                }
+            }
+            if (bookedRoom == null)
+            {
+                Console.WriteLine("No rooms available now.");
+            }
+            else
+            {
+                Console.WriteLine("Hurray, you got a room - " + bookedRoom);
+            }
+        }
+
+        void Checkout(string room)
+        {
+            this._setStatus(room, Status.Occupied, Status.Vacant);
+        }
+
+        void Clean(string room)
+        {
+            this._setStatus(room, Status.Vacant, Status.Available);
+        }
+
+        void Repair(string room)
+        {
+            this._setStatus(room, Status.Vacant, Status.Repair);
+        }
+
+        void Fix(string room)
+        {
+            this._setStatus(room, Status.Repair, Status.Vacant);
+        }
+
+        void _setStatus(string room, Status fromStatus, Status toStatus)
+        {
+            if (Rooms.ContainsKey(room))
+            {
+                var status = Rooms[room];
+                if (status == fromStatus)
+                {
+                    Rooms[room] = toStatus;
+                    Console.WriteLine("Command accepted");
+                }
+                else
+                {
+                    Console.WriteLine("Command rejected");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid room number.");
+            }
+        }
+        public void ProcessCommand(string command)
+        {
+            if (command == GlobalConstants.EXIT)
+            {
+                Environment.Exit(0);
+            }
+            else
+            {
+                var hotel = Hotel.Instance;
+                var inputs = command.Split(' ');
+                var cmd = inputs[0];
+                switch (cmd)
+                {
+                    case "list":
+                        hotel.Display();
+                        break;
+                    case "book":
+                        hotel.Book();
+                        break;
+                    case "checkout":
+                        hotel.Checkout(inputs[1]);
+                        break;
+                    case "clean":
+                        hotel.Clean(inputs[1]);
+                        break;
+                    case "repair":
+                        hotel.Repair(inputs[1]);
+                        break;
+                    case "fix":
+                        hotel.Fix(inputs[1]);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid command");
+                        break;
+                }
+            }
+        }
+    }
+
+    enum Status
+    {
+        Available = 1,
+        Occupied = 2,
+        Vacant = 3,
+        Repair = 4
+    }
+
+    class GlobalConstants
+    {
+        public const string EXIT = "exit";
+    }
+
+    interface ICommand
+    {
+        void ProcessCommand(string command);
+    }
+}
